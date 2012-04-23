0892139738fbdff8f9c216ceb5159c76d557aa1a
David Sanderson <dont_sp@am.me>
2012-04-23 19:16:44 +0100

    Removed todo that was done

--- src/main/java/uk/ac/imperial/dws04/LaneMoveHandler.java
+++ src/main/java/uk/ac/imperial/dws04/LaneMoveHandler.java
@@ -98,7 +98,6 @@ public class LaneMoveHandler extends MoveHandler {
 		CellMove m = (CellMove) action;
 
 		// TODO - requires environment services for agents' max speed etc.
-		// check move forward magnitude is <= actor's max speed
 		// check move forward change in magnitude <= actor's max
 		// acceleration/deceleration
 		// check agent only moving in/out of lane0 when off/on ramp is present 
