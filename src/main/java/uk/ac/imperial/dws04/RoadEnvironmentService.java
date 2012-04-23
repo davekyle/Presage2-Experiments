4b2a001d5e42da70f86cf409897106ee8b73f2b6
David Sanderson <dont_sp@am.me>
2012-04-23 17:40:57 +0100

    Actually adding the environment service...

---
+++ src/main/java/uk/ac/imperial/dws04/RoadEnvironmentService.java
@@ -0,0 +1,57 @@
+/**
+ * See http://www.presage2.info/ for more details on Presage2
+ */
+package uk.ac.imperial.dws04;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import uk.ac.imperial.presage2.core.environment.EnvironmentService;
+import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
+
+/**
+ * Class to hold global shared state variables for the RoadSimulation.
+ * Currently creates empty junctionLocations list if no junctions. 
+ * 
+ * @author dws04
+ *
+ */
+public class RoadEnvironmentService extends EnvironmentService {
+
+	private final Logger logger = Logger.getLogger(RoadEnvironmentService.class);
+	private final ArrayList<Integer> junctionLocations;
+	
+	@Inject
+	protected RoadEnvironmentService(EnvironmentSharedStateAccess sharedState,
+			@Named("params.length") int length, @Named("params.junctionCount") int junctionCount,
+			@Named("params.lanes") int lanes, @Named("params.maxSpeed") int maxSpeed,
+			@Named("params.maxAccel") int maxAccel, @Named("params.maxDeccel") int maxDeccel){
+		super(sharedState);
+		junctionLocations = createJunctionList(length, junctionCount);
+		sharedState.createGlobal("junctionLocations", junctionLocations);
+	}
+	
+	/**
+	 * Creates list of locations at which to place evenly-spaced junctions. (length*(0->jcCount/jCount+1) 
+	 * @param length
+	 * @param junctionCount
+	 * @return
+	 */
+	private final ArrayList<Integer> createJunctionList(int length, int junctionCount) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if (junctionCount!=0){
+			// length*(i/(junctionCount+1) -> (length/(junctionCount+1))*i to avoid Integer rounding
+			for (int i = 1; i<=junctionCount; i++) {
+				//Integer temp = length*(i/(junctionCount+1));
+				result.add((length/(junctionCount+1))*i);
+				logger.trace("Added a junction at " + (length/(junctionCount+1))*i);
+			}
+		}
+		return result;
+	}
+
+}
