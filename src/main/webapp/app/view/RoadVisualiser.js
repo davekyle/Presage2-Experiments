eb5f5ed20f0c9b14c83b2ef5ad3721ddbf11331c
Sam Macbeth <sm1106@imperial.ac.uk>
2012-05-18 17:03:32 +0100

    Use offset to centre agents on visualiser.

--- src/main/webapp/app/view/RoadVisualiser.js
+++ src/main/webapp/app/view/RoadVisualiser.js
@@ -2,6 +2,9 @@ Ext.define('Presage2.view.RoadVisualiser', {
 	extend: 'Presage2.view.2DVisualiser',
 	alias: 'widget.roadvisualiser',
 	initComponent: function() {
+		Ext.apply(this, {
+			xOffset: 200
+		});
 		this.callParent(arguments);
 	},
 	getScale: function(simulation) {
@@ -17,11 +20,11 @@ Ext.define('Presage2.view.RoadVisualiser', {
 			width: this.scale * 0.7,
 			height: this.scale,
 			fill: '#111',
-			x: 200 + (ag.data.data.x * this.scale),
-			y: 10 + (ag.data.data.y * this.scale)
+			x: this.xOffset + (ag.data.data.x * this.scale),
+			y: this.yOffset + (ag.data.data.y * this.scale)
 		};
 		return agent;
-	},
+	}/*,
 	setTimeStep: function(time) {
 		var step = this.timeline.getById(time);
 		if(step != null) {
@@ -35,5 +38,5 @@ Ext.define('Presage2.view.RoadVisualiser', {
 				}
 			}, this);
 		}
-	},
+	}*/
 });
