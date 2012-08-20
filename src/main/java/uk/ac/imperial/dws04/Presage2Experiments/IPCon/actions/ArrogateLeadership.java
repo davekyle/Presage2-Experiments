f43e8adb8751c0d8ef5fc3fda0c9abddfdf2d941
davekyle <dont_sp@am.me>
2012-08-20 18:45:13 +0100

    Vote, Arrogate, Resign

---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/ArrogateLeadership.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions;
+
+import java.util.UUID;
+
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+
+/**
+ * @author dave
+ *
+ */
+public class ArrogateLeadership {
+	public NetworkAddress agent;
+	public Integer revision;
+	public String issue;
+	public UUID cluster;
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agent == null) ? 0 : agent.hashCode());
+		result = prime * result + ((cluster == null) ? 0 : cluster.hashCode());
+		result = prime * result + ((issue == null) ? 0 : issue.hashCode());
+		result = prime * result
+				+ ((revision == null) ? 0 : revision.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArrogateLeadership other = (ArrogateLeadership) obj;
+		if (agent == null) {
+			if (other.agent != null)
+				return false;
+		} else if (!agent.equals(other.agent))
+			return false;
+		if (cluster == null) {
+			if (other.cluster != null)
+				return false;
+		} else if (!cluster.equals(other.cluster))
+			return false;
+		if (issue == null) {
+			if (other.issue != null)
+				return false;
+		} else if (!issue.equals(other.issue))
+			return false;
+		if (revision == null) {
+			if (other.revision != null)
+				return false;
+		} else if (!revision.equals(other.revision))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/Vote2B.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions;
+
+import java.util.UUID;
+
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+
+/**
+ * @author dave
+ *
+ */
+public class Vote2B {
+	public NetworkAddress agent;
+	public Integer revision;
+	public Integer ballot;
+	public Object value;
+	public String issue;
+	public UUID cluster;
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agent == null) ? 0 : agent.hashCode());
+		result = prime * result + ((ballot == null) ? 0 : ballot.hashCode());
+		result = prime * result + ((cluster == null) ? 0 : cluster.hashCode());
+		result = prime * result + ((issue == null) ? 0 : issue.hashCode());
+		result = prime * result
+				+ ((revision == null) ? 0 : revision.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vote2B other = (Vote2B) obj;
+		if (agent == null) {
+			if (other.agent != null)
+				return false;
+		} else if (!agent.equals(other.agent))
+			return false;
+		if (ballot == null) {
+			if (other.ballot != null)
+				return false;
+		} else if (!ballot.equals(other.ballot))
+			return false;
+		if (cluster == null) {
+			if (other.cluster != null)
+				return false;
+		} else if (!cluster.equals(other.cluster))
+			return false;
+		if (issue == null) {
+			if (other.issue != null)
+				return false;
+		} else if (!issue.equals(other.issue))
+			return false;
+		if (revision == null) {
+			if (other.revision != null)
+				return false;
+		} else if (!revision.equals(other.revision))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+}
