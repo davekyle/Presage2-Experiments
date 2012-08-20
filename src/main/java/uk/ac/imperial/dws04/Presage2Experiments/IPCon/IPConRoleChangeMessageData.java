0891b883f784bb97fe9e5a544a6e8e40ade32580
davekyle <dont_sp@am.me>
2012-08-20 15:35:57 +0100

    Refactoring out the role change message stuff
    Not sure if this had any point but made sense at the time...

---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/IPConRoleChangeMessageData.java
@@ -0,0 +1,83 @@
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon;
+
+import uk.ac.imperial.dws04.Presage2Experiments.IPCon.IPConProtocol.Role;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+
+public class IPConRoleChangeMessageData {
+	private final NetworkAddress agentId;
+	private final String issue;
+	/**
+	 * true if added, false if removed
+	 */
+	private final Boolean addRole;
+	/**
+	 * The agent's old role
+	 */
+	private final Role oldRole;
+	/**
+	 * The agent's new role
+	 */
+	private final Role newRole;
+	
+	private final Integer ballotNum;
+	
+	/**
+	 * @param agentId
+	 * @param issue
+	 * @param addRole
+	 * @param oldRole
+	 * @param newRole
+	 */
+	public IPConRoleChangeMessageData(NetworkAddress agentId, String issue, Integer ballotNum,
+			Boolean addRole, Role oldRole, Role newRole) {
+		super();
+		this.agentId = agentId;
+		this.issue = issue;
+		this.ballotNum = ballotNum;
+		this.addRole = addRole;
+		this.oldRole = oldRole;
+		this.newRole = newRole;
+	}
+
+	/**
+	 * @return the agentId
+	 */
+	public NetworkAddress getAgentId() {
+		return agentId;
+	}
+
+	/**
+	 * @return the issue
+	 */
+	public String getIssue() {
+		return issue;
+	}
+
+	/**
+	 * @return the addRole
+	 */
+	public Boolean getAddRole() {
+		return addRole;
+	}
+
+	/**
+	 * @return the oldRole
+	 */
+	public Role getOldRole() {
+		return oldRole;
+	}
+
+	/**
+	 * @return the newRole
+	 */
+	public Role getNewRole() {
+		return newRole;
+	}
+
+	/**
+	 * @return the ballotNum
+	 */
+	public Integer getBallotNum() {
+		return ballotNum;
+	}
+}
---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/Messages/ChangeRoleMessage.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.Messages;
+
+import uk.ac.imperial.dws04.Presage2Experiments.IPCon.IPConProtocol.MessageType;
+import uk.ac.imperial.dws04.Presage2Experiments.IPCon.IPConProtocol.Role;
+import uk.ac.imperial.dws04.Presage2Experiments.IPCon.IPConRoleChangeMessageData;
+import uk.ac.imperial.presage2.core.messaging.Performative;
+import uk.ac.imperial.presage2.core.network.BroadcastMessage;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+import uk.ac.imperial.presage2.core.simulator.SimTime;
+import uk.ac.imperial.presage2.util.protocols.FSMConversation;
+
+/**
+ * @author dave
+ *
+ */
+public class ChangeRoleMessage extends BroadcastMessage<IPConRoleChangeMessageData> {
+	
+	ChangeRoleMessage(FSMConversation conv, NetworkAddress agentId, String issue, Integer ballotNum, Boolean addOrRem, Role oldRole, Role newRole) {
+		super(Performative.INFORM, MessageType.INTERNAL_ROLE_CHANGE.name(), SimTime.get(), conv.getNetwork().getAddress(), new IPConRoleChangeMessageData(agentId, issue, ballotNum, addOrRem, oldRole, newRole));
+	}
+	
+	/**
+	 * @return the agentId
+	 */
+	public NetworkAddress getAgentId() {
+		return data.getAgentId();
+	}
+
+	/**
+	 * @return the issue
+	 */
+	public String getIssue() {
+		return data.getIssue();
+	}
+
+	/**
+	 * @return the addRole
+	 */
+	public Boolean getAddRole() {
+		return data.getAddRole();
+	}
+
+	/**
+	 * @return the oldRole
+	 */
+	public Role getOldRole() {
+		return data.getOldRole();
+	}
+
+	/**
+	 * @return the newRole
+	 */
+	public Role getNewRole() {
+		return data.getNewRole();
+	}
+
+	/**
+	 * Don't think this is relevant
+	 * @return the ballotNum
+	 */
+	@Deprecated
+	public Integer getBallotNum() {
+		return data.getBallotNum();
+	}
+}
