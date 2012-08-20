5ca6a754fe15ece56921ae6c467b24a9d5172f68
davekyle <dont_sp@am.me>
2012-08-20 19:33:45 +0100

    AddRole and RemRole

---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/RemRole.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions;
+
+import java.util.UUID;
+
+import uk.ac.imperial.dws04.Presage2Experiments.IPCon.IPConProtocol.Role;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+
+/**
+ * @author dave
+ *
+ */
+public class RemRole {
+	public NetworkAddress leader;
+	public NetworkAddress agent;
+	public Role role;
+	public Integer revision;
+	public String issue;
+	public UUID cluster;
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agent == null) ? 0 : agent.hashCode());
+		result = prime * result + ((cluster == null) ? 0 : cluster.hashCode());
+		result = prime * result + ((issue == null) ? 0 : issue.hashCode());
+		result = prime * result + ((leader == null) ? 0 : leader.hashCode());
+		result = prime * result
+				+ ((revision == null) ? 0 : revision.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RemRole other = (RemRole) obj;
+		if (agent == null) {
+			if (other.agent != null)
+				return false;
+		} else if (!agent.equals(other.agent))
+			return false;
+		if (cluster == null) {
+			if (other.cluster != null)
+				return false;
+		} else if (!cluster.equals(other.cluster))
+			return false;
+		if (issue == null) {
+			if (other.issue != null)
+				return false;
+		} else if (!issue.equals(other.issue))
+			return false;
+		if (leader == null) {
+			if (other.leader != null)
+				return false;
+		} else if (!leader.equals(other.leader))
+			return false;
+		if (revision == null) {
+			if (other.revision != null)
+				return false;
+		} else if (!revision.equals(other.revision))
+			return false;
+		if (role != other.role)
+			return false;
+		return true;
+	}
+}
