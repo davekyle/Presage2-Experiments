8383058929106106a0f8472255c0c956088d4734
davekyle <dont_sp@am.me>
2012-09-07 15:54:36 +0100

    More tests
    Checked a lot more factcounts; looks like adding a Cluster fact like an
    IPConAgent fact would be helpful in some cases...

--- src/test/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/IPConDrlsTest.java
+++ src/test/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/IPConDrlsTest.java
@@ -5,12 +5,9 @@ package uk.ac.imperial.dws04.Presage2Experiments.IPCon;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
@@ -31,7 +28,6 @@ import uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions.Response1B;
 import uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions.Submit2A;
 import uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions.Vote2B;
 import uk.ac.imperial.dws04.Presage2Experiments.IPCon.facts.IPConAgent;
-import uk.ac.imperial.presage2.core.network.NetworkAddress;
 import uk.ac.imperial.presage2.core.util.random.Random;
 import uk.ac.imperial.presage2.rules.RuleModule;
 import uk.ac.imperial.presage2.rules.RuleStorage;
@@ -92,12 +88,36 @@ public class IPConDrlsTest {
 	public final Collection<Object> assertFactCount(final String factTypeString, int count) {
 		final FactType factType = typeFromString(factTypeString);
 		
-		Collection<Object> facts = session.getObjects(new ObjectFilter() {
+		// try drls fact types
+		Collection<Object> facts = new HashSet<Object>();
+		facts.addAll(session.getObjects(new ObjectFilter() {
 			@Override
 			public boolean accept(Object object) {
 				return assertFactType(object, factType);
 			}
-		});
+		}));
+		if (facts.size()==0) {
+			// try java classes
+			facts.addAll(session.getObjects(new ObjectFilter() {
+	
+				@Override
+				public boolean accept(Object object) {
+					Class<?> c;
+					try {
+						c = Class.forName("uk.ac.imperial.dws04.Presage2Experiments.IPCon.facts." + factTypeString);
+						logger.info("Testing " + object + " which is a " + object.getClass());
+						return c.isInstance(object);
+					} catch (NullPointerException e) {
+						//e.printStackTrace();
+						return false;
+					} catch (ClassNotFoundException e) {
+						//e.printStackTrace();
+						return false;
+					}
+				}
+				
+			}));
+		}
 		assertEquals(count, facts.size());
 		return facts;
 	}
@@ -130,6 +150,87 @@ public class IPConDrlsTest {
 		Role role = (Role) typeFromString("HasRole").get(hasRole, "role");
 		assertEquals(role.toString(), "LEADER");
 		logger.info("Finished basicTest()\n");
+	}
+	
+	/**
+	 * Test to check the power, permission, and obligations around Response1B action
+	 * 
+	 * Pre: Acceptor will be inserted with a previous vote and a pre_vote will be asserted
+	 * Test: Check the agent has power to respond, permission to respond correctly, no permission to respond incorrectly 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void checkResponsePowPerObl() throws Exception {
+		logger.info("\nStarting checkResponsePowPerObl()");
+		IPConAgent a1 = new IPConAgent("a1");
+		Integer revision = 1;
+		Integer ballot = 10;
+		Object v1 = "1";
+		Object v2 = "v2";
+		Object v3 = 3;
+		String issue = "IssueString";
+		UUID cluster = Random.randomUUID();
+		session.insert(a1);
+		// Insert intially facts:
+		// Agent is acceptor
+		FactType roleType = typeFromString("HasRole");
+		Object role = roleType.newInstance();
+		roleType.set(role, "role", Role.ACCEPTOR);
+		roleType.set(role, "agent", a1);
+		roleType.set(role, "revision", revision);
+		roleType.set(role, "issue", issue);
+		roleType.set(role, "cluster", cluster);
+		session.insert(role);
+		// PreVote exists
+		FactType preVoteType = typeFromString("Pre_Vote");
+		Object preVote = preVoteType.newInstance();
+		preVoteType.set(preVote, "revision", revision);
+		preVoteType.set(preVote, "ballot", ballot);
+		preVoteType.set(preVote, "issue", issue);
+		preVoteType.set(preVote, "cluster", cluster);
+		session.insert(preVote);
+		// Agent voted in 3 previous ballots
+		final FactType votedType = typeFromString("Voted");
+		Object v1Vote = votedType.newInstance();
+		votedType.set(v1Vote, "agent", a1);
+		votedType.set(v1Vote, "revision", revision);
+		votedType.set(v1Vote, "ballot", ballot-5);
+		votedType.set(v1Vote, "value", v1);
+		votedType.set(v1Vote, "issue", issue);
+		votedType.set(v1Vote, "cluster", cluster);
+		session.insert(v1Vote);
+		Object v2Vote = votedType.newInstance();
+		votedType.set(v2Vote, "agent", a1);
+		votedType.set(v2Vote, "revision", revision);
+		votedType.set(v2Vote, "ballot", ballot-3);
+		votedType.set(v2Vote, "value", v2);
+		votedType.set(v2Vote, "issue", issue);
+		votedType.set(v2Vote, "cluster", cluster);
+		session.insert(v2Vote);
+		Object v3Vote = votedType.newInstance();
+		votedType.set(v3Vote, "agent", a1);
+		votedType.set(v3Vote, "revision", revision);
+		votedType.set(v3Vote, "ballot", ballot-2);
+		votedType.set(v3Vote, "value", v3);
+		votedType.set(v3Vote, "issue", issue);
+		votedType.set(v3Vote, "cluster", cluster);
+		session.insert(v3Vote);
+		
+		rules.incrementTime();
+		
+		/*
+		 * Make sure initially facts hold
+		 */
+		// agent is inserted
+		assertFactCount("IPConAgent", 1);
+		assertFactCount("HasRole", 1);
+		assertFactCount("Pre_Vote", 1);
+		assertFactCount("Voted", 3);
+		
+
+		session.insert(new Response1B( a1, 1, 0, null, 1, 10, issue, cluster));
+		
 	}
 	
 	@Test
@@ -199,11 +300,52 @@ public class IPConDrlsTest {
 		
 		// check there are the right number of roles
 		// 5 acceptors, one leader, one proposer
+		assertFactCount("IPConAgent", 5);
 		assertFactCount("HasRole", 7);
 		
 		// check theres only one agent can request (the proposer)
 		// one proposer has permission
 		assertFactCount("RequestPer", 1);
+		
+		
+		/*
+		 *  check some arbitrary fact counts
+		 */
+		// everyone can arrogate (leader could arrogate something else for example)
+		assertFactCount("ArrogatePer", 5);
+		// only leader can resign
+		assertFactCount("ResignPow", 1);
+		// all can leave
+		// FIXME TODO need to fix this holdsAt so each role doesn't count
+		assertFactCount("LeavePow", 7);
+		/*
+		 * 5 roles in total
+		 * leader can add roles:
+		 * 2 for himself
+		 * (4 for others)*4 = 16
+		 * Total: 18
+		 */
+		assertFactCount("AddRolePow", 18);
+		assertFactCount("AddRolePer", 18);
+		/*
+		 * 5 roles in total
+		 * leader can rem roles:
+		 * 3 for himself
+		 * (1 for others)*4 = 4
+		 * Total: 7
+		 */
+		assertFactCount("RemRolePow", 7);
+		assertFactCount("RemRolePer", 7);
+		// leader can revise
+		assertFactCount("RevisePow", 1);
+		assertFactCount("RevisePer", 1);
+		// leader has pow to syncreq all acceptors (incl himself)
+		assertFactCount("SyncReqPow", 5);
+		// but not the permission
+		assertFactCount("SyncReqPer", 0);
+		//no one can syncack
+		assertFactCount("SyncAckPow", 0);
+		assertFactCount("SyncAckPer", 0);
 		
 		/*
 		 * Begin the time steps.
@@ -219,6 +361,7 @@ public class IPConDrlsTest {
 		// check no one can response
 		assertFactCount("ResponsePer", 0);
 		
+		
 		/*
 		 * Time step 2 :
 		 * leader issues prepare, check 5 acceptors can respond
@@ -229,6 +372,15 @@ public class IPConDrlsTest {
 		// check all acceptors can response
 		// 5 acceptors have permission
 		assertFactCount("ResponsePer", 5);
+		if (pass) {
+			// 2 acceptors have to respond
+			assertFactCount("Obligation", 2);
+		}else {
+			// 3 have to
+			assertFactCount("Obligation", 3);
+		}
+		
+		
 		/*
 		 * Time step 3 :
 		 * All agents send a response, check the responsecount and per/obl to submit
@@ -287,10 +439,24 @@ public class IPConDrlsTest {
 		
 		/*
 		 * Time step 5
-		 * Remaining 3 agents vote correctly
-		 * Check vote counts and correct chosen
+		 * Agent 3 votes correctly, 3 agents have now voted, so chosen
+		 * Check correct value has been chosen
 		 */
 		session.insert( new Vote2B(a3, 1, 10, 3, issue, cluster));
+		rules.incrementTime();
+		// value was chosen
+		Collection<Object> chosens = assertFactCount("Chosen", 1);
+		Object chosen = Arrays.asList(chosens.toArray()).get(0);
+		Object value = typeFromString("Chosen").get(chosen, "value");
+		// correct value was chosen
+		assertEquals(value, 3);
+		
+		
+		/*
+		 * Time step 6
+		 * Remaining 2 agents vote correctly
+		 * Check vote counts and correct chosen
+		 */
 		session.insert( new Vote2B(a4, 1, 10, 3, issue, cluster));
 		session.insert( new Vote2B(a5, 1, 10, 3, issue, cluster));
 		rules.incrementTime();
@@ -352,7 +518,7 @@ public class IPConDrlsTest {
 		try {
 			return ( ( factType.newInstance().getClass() ).equals( ( obj.getClass() ) ) );
 		} catch (NullPointerException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			return false;
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
