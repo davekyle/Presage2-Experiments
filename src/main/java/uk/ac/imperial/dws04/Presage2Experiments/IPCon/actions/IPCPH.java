89e3a392960a8ec668a551f957339a5b53d7e079
davekyle <dont_sp@am.me>
2012-09-20 16:56:33 +0100

    Moved Obl fact

---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/IPCPH.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions;
+
+/**
+ * Placeholder object for IPCon
+ * 
+ * @author dws04
+ *
+ */
+public class IPCPH extends Object {
+	public IPCPH(){
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "#:IPCon_Placeholder:#";
+	}
+}
