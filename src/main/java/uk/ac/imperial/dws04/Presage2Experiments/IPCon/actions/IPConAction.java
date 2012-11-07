cb3c9df223e8a6cc37de311eda657c3278d69570
davekyle <dws004+github@googlemail.com>
2012-11-07 17:34:43 +0000

    Unabstracted fulfils() code
    Could be finalised as well, but for the time being leaving it here...

--- src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/IPConAction.java
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/actions/IPConAction.java
@@ -3,6 +3,8 @@
  */
 package uk.ac.imperial.dws04.Presage2Experiments.IPCon.actions;
 
+import java.lang.reflect.Field;
+
 /**
  * @author dws04
  *
@@ -11,10 +13,35 @@ public abstract class IPConAction {
 	
 	/**
 	 * Indicates whether this action *could* fulfil an obligation to perform the given action (also requires other checks on the obl)
-	 * @param action
+	 * @param obligation
 	 * @return true if this action is "the same as" the argument with the exception of the agent
 	 */
-	public abstract boolean fulfils(IPConAction action);
+	public final boolean fulfils(IPConAction obligation) {
+		// check for equality
+		if (this.equals(obligation)) return true;
+		// check for different class
+		else if (!(this.getClass().isAssignableFrom(obligation.getClass()))) return false;
+		else {
+			boolean result = true;
+			// check each field is either null or the same
+			for (Field f : this.getClass().getFields()) {
+				if (!result) return false;
+				else {
+					try {
+						result = result && (
+								(f.get(obligation).equals(null)) || 
+								(f.get(this).equals(f.get(obligation)))
+								);
+					} catch (Exception e) {
+						e.printStackTrace();
+						return false;
+					}
+				}
+			}
+			return result;
+		}
+		
+	}
 	
 	/**
 	 * Copy constructor...
