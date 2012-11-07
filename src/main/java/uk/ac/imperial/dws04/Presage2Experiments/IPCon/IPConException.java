ce9fa940c3e40596ade5216fc40da96810d1b755
davekyle <dws004+github@googlemail.com>
2012-11-07 18:17:28 +0000

    Renamed exception to be more generic

--- src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/IPConException.java
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/IPConException.java
@@ -7,18 +7,18 @@ package uk.ac.imperial.dws04.Presage2Experiments.IPCon;
  * @author dws04
  *
  */
-public class IPCNV_Exception extends Exception {
+public class IPConException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3190237122473048497L;
 
-	public IPCNV_Exception(String string) {
+	public IPConException(String string) {
 		super(string);
 	}
 
-	public IPCNV_Exception() {
+	public IPConException() {
 		// TODO Auto-generated constructor stub
 	}
 
