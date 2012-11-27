d90ec71b0e4126d9132e833ec1e366a0d613afef
davekyle <dws004+github@googlemail.com>
2012-11-27 12:07:09 +0000

    Made a "constraint" to insert IPConAction

---
+++ src/main/java/uk/ac/imperial/dws04/Presage2Experiments/IPCon/Messages/IPConMsgToRuleEngine.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package uk.ac.imperial.dws04.Presage2Experiments.IPCon.Messages;
+
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import uk.ac.imperial.presage2.core.network.Message;
+import uk.ac.imperial.presage2.rules.MessagesToRuleEngine;
+
+/**
+ * @author dws04
+ *
+ */
+public class IPConMsgToRuleEngine extends MessagesToRuleEngine {
+
+	private final StatefulKnowledgeSession sessionPtr;
+
+	public IPConMsgToRuleEngine(StatefulKnowledgeSession session) {
+		super(session);
+		sessionPtr = session;
+	}
+	
+	/**
+	 * Inserts the IPConAction to the kbase if the message is an instanceof IPConActionMsg,
+	 * otherwise inserts the message to the kbase.
+	 * 
+	 * @param m Message to be processed
+	 * @return Message the message that was processed
+	 * @see uk.ac.imperial.presage2.rules.MessagesToRuleEngine#constrainMessage(uk.ac.imperial.presage2.core.network.Message)
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Message constrainMessage(Message m){
+		if (m instanceof IPConActionMsg) {
+			sessionPtr.insert(((IPConActionMsg)m).getData());
+			return m;
+		}
+		else return super.constrainMessage(m);
+	}
+
+}
