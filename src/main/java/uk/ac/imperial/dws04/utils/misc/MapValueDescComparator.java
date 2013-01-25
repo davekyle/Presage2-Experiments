dc9f86d012588feba61240ae74457e75b59e643a
davekyle <dws004+github@googlemail.com>
2013-01-25 19:20:06 +0000

    Made a comparator for maps to sort by value

---
+++ src/main/java/uk/ac/imperial/dws04/utils/misc/MapValueDescComparator.java
@@ -0,0 +1,33 @@
+package uk.ac.imperial.dws04.utils.misc;
+/**
+ * 
+ */
+
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * @author dws04
+ *
+ */
+public class MapValueDescComparator<V extends Comparable<? super V>> implements Comparator<Map.Entry<?, V>> {
+
+	/** 
+	 * Note: this class has a natural ordering that is inconsistent with equals.
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(Entry<?, V> arg0, Entry<?, V> arg1) {
+		int compare = arg0.getValue().compareTo(arg1.getValue());
+		if (compare<0) {
+			return compare;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+
+}
