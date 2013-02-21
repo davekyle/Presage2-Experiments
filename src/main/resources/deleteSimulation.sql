a217db26ed15a0e107f2ce3645d8ce1d8dccc6e2
davekyle <dws004+github@googlemail.com>
2013-02-21 17:20:23 +0000

    SQL code to delete a whole sim

---
+++ src/main/resources/deleteSimulation.sql
@@ -0,0 +1,5 @@
+DELETE FROM agenttransient WHERE "simId" = 20;
+DELETE FROM agents WHERE "simId" = 20;
+DELETE FROM environmenttransient WHERE "simId" = 20;
+DELETE FROM environment WHERE "simId" = 20;
+DELETE FROM simulations WHERE "id" = 20;
