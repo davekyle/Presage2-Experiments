6af5213a1fb60743f6bd53e71a47d1f991283499
davekyle <dws004+github@googlemail.com>
2013-02-21 12:12:03 +0000

    Added a script to delete the GOALS entries in the db

---
+++ src/main/resources/deleteGOALS.sql
@@ -0,0 +1,5 @@
+﻿DELETE FROM agenttransient WHERE "simId" IN (SELECT id FROM simulations WHERE (parameters->'ownChoiceMethod' = 'GOALS'));
+DELETE FROM agents WHERE "simId" IN (SELECT id FROM simulations WHERE (parameters->'ownChoiceMethod' = 'GOALS'));
+DELETE FROM environmenttransient WHERE "simId" IN (SELECT id FROM simulations WHERE (parameters->'ownChoiceMethod' = 'GOALS'));
+DELETE FROM environment WHERE "simId" IN (SELECT id FROM simulations WHERE (parameters->'ownChoiceMethod' = 'GOALS'));
+DELETE FROM simulations WHERE "id" IN (SELECT id FROM simulations WHERE (parameters->'ownChoiceMethod' = 'GOALS'));
